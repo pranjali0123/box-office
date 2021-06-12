fbf5296592b5200be6bc8ce12174e4da41f620fb
pranjali0123 <pranjali2301@gmail.com>
2021-06-12 22:19:31 +0530

    created two new more hooks:useLastQuerry and useShow

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,10 @@ import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
 import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
+import { useLastQuerry } from '../misc/custom-hooks';
 
 const Home = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useLastQuerry();
   const [results, setResults] = useState(null);
   const [searchOption, setSearchOption] = useState('shows');
 
